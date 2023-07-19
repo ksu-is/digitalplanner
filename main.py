5a1bd08c5a59cebc640cd2c19076b51f51a87bae
AlexaRGuerra <134737558+AlexaRGuerra@users.noreply.github.com>
2023-07-18 23:41:34 -0400

    Update main.py
    added display_task to checks if the date argument exists as a key . If the date appears it means that there are tasks affiliated with the date.However, if the date is not found it means there are no tasks for the specific date. If tasks are found for the date provided it will print a message indicating the date and 
     proceeds to repeat over the tasks using a loop.
    Inside the loop, it  will print each task with a corresponding number which will start at 1 using the enumerate() function.
    As well, if there are no tasks for the given date, it prints a message that says no tasks were found.

--- main.py
+++ main.py
@@ -9,13 +9,24 @@ class DigitalPlanner:
         if date not in self.tasks:
             self.tasks[date] = []
         self.tasks[date].append(task)
-
+    
     def remove_task(self, date, task):
         if date in self.tasks:
             if task in self.tasks[date]:
                 self.tasks[date].remove(task)
                 if len(self.tasks[date]) == 0:
                     del self.tasks[date]
+    
+    def display_tasks(self, date):
+        if date in self.tasks:
+            print(f"Tasks for {date}:")
+            for i, task in enumerate(self.tasks[date], 1):
+                print(f"{i}. {task}")
+        else:
+            print("No tasks found for this date.")
+   
+                       
+                    
 
 
 
