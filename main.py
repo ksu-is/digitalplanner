afd09a058e81e929a3e9169b2be5ae68f8baa10c
AlexaRGuerra <134737558+AlexaRGuerra@users.noreply.github.com>
2023-07-18 23:32:26 -0400

    Update main.py
    added the remove_task in order to remove a task that is associated with a specific date from the tasks dictionary. Additionally, after removing the task if there is empty (0) the date no longer is use. As a result, deleting the key-pair value from the tasks dictionary.

--- main.py
+++ main.py
@@ -10,5 +10,12 @@ class DigitalPlanner:
             self.tasks[date] = []
         self.tasks[date].append(task)
 
+    def remove_task(self, date, task):
+        if date in self.tasks:
+            if task in self.tasks[date]:
+                self.tasks[date].remove(task)
+                if len(self.tasks[date]) == 0:
+                    del self.tasks[date]
+
 
 
