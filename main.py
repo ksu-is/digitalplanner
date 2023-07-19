48b0a89a26b3d19ecf17ca8334b17de28a582c6a
AlexaRGuerra <134737558+AlexaRGuerra@users.noreply.github.com>
2023-07-18 23:26:58 -0400

    Update main.py
    added the add_task  in order to be able to add a task while also organizing them by date. If the date is not to be found it will create a new entry with an empty list. If the date does in fact already exist it will append the task to the already existing list for the specific date.

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import calendar
 
 class DigitalPlanner:
-  def_init_(self):
-  self.calendar = calendar.calendar()
-  self.taks. ={}
-
-  
-
+    def __init__(self):
+        self.calendar = calendar.Calendar()
+        self.tasks = {}
+    
+    def add_task(self, date, task):
+        if date not in self.tasks:
+            self.tasks[date] = []
+        self.tasks[date].append(task)
 
 
 
